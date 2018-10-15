e542a837a024291179562cfa815266253a8b1867
Hrobot <Hrobot@users.noreply.github.com>
2018-10-15 21:06:09 +0800

    Update def add_layer().py

--- def add_layer().py
+++ def add_layer().py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-def add_layer(inputs, in_size, activation_function=None);
+def add_layer(inputs, in_size, activation_function=None):
   Weights = tf.Variable(tf.random_normal([in_size, out_size]));
   biases = tf.Variable(tf.zeros([1, out_size]) +0.1);
   Wx_plus_b = tf.matmul(inputs, Weights) + biases
